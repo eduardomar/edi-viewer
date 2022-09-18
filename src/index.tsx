7de3edca200ac64b5cc60aca8f5be87d83d0d944
Eduardo Ramírez <eramirez@freshsoftwareconcepts.com>
2022-09-18 17:37:36 -0500

    Add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StandalonePN from './pages/StandalonePN';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+  {
+    path: '/standalone-pn',
+    element: <StandalonePN />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
---
+++ src/pages/StandalonePN/StandalonePN.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StandalonePN: React.FC = () => {
+  return <div>StandalonePN</div>;
+};
+
+export default StandalonePN;
