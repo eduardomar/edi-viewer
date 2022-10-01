4dd42b30fa6c6ddd0181a8932d8bb5f1bc103b5d
Eduardo Ramírez <eramirez@freshsoftwareconcepts.com>
2022-10-01 13:38:36 -0500

    Add segments 23, 31, and 40, for Entry Summary

--- src/records/Entry Summary/AE/index.ts
+++ src/records/Entry Summary/AE/index.ts
@@ -4,6 +4,9 @@ const AE: RecordJSON[] = [
   require('./11.json'),
   require('./20.json'),
   require('./22.json'),
+  require('./23.json'),
+  require('./31.json'),
+  require('./40.json'),
 ];
 
 export default AE;
