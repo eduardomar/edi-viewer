99ed8da57462db2b853b3082228b9c263812e9ed
Eduardo Ramírez <eramirez@freshsoftwareconcepts.com>
2022-12-13 20:58:36 -0600

    Add segments 21, SE13 and SE16, for Entry Summary

--- src/records/Entry Summary/AE/index.ts
+++ src/records/Entry Summary/AE/index.ts
@@ -3,6 +3,7 @@ const AE: RecordJSON[] = [
   require('./10.json'),
   require('./11.json'),
   require('./20.json'),
+  require('./21.json'),
   require('./22.json'),
   require('./23.json'),
   require('./31.json'),
@@ -14,6 +15,8 @@ const AE: RecordJSON[] = [
   require('./62.json'),
   require('./89.json'),
   require('./90.json'),
+  require('./SE13.json'),
+  require('./SE16.json'),
   require('./SE50.json'),
 ];
 
