5def5fb01cfa581c52b0bbebbc1d0d81045cf619
pablo-clemente <pablocm1998@gmail.com>
2023-02-02 16:52:33 -0600

    add segments K2 to K8, for Importer Query

--- src/records/Importer Query/KI/index.ts
+++ src/records/Importer Query/KI/index.ts
@@ -1,4 +1,14 @@
 import RecordJSON from '../../../interfaces/RecordJSON';
-const KI: RecordJSON[] = [require('./K.json'), require('./K1.json')];
+const KI: RecordJSON[] = [
+  require('./K.json'),
+  require('./K1.json'),
+  require('./K2.json'),
+  require('./K3.json'),
+  require('./K4.json'),
+  require('./K5.json'),
+  require('./K6.json'),
+  require('./K7.json'),
+  require('./K8.json'),
+];
 
 export default KI;
